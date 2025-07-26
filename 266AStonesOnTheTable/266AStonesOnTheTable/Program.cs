77d4bcd28f73cf0ca1c5b0ce6dc304c114556534
Lord Liberté <GhostRg110@gmail.com>
2025-07-26 16:37:09 +0200

    Add solutions for 266A and 791A Codeforces problems
    Added new C# projects for the problems 266A (Stones on the Table) and 791A (Bear and Big Brother), including solution files, project files, source code, and configuration. Also updated Visual Studio workspace and project metadata files.

---
+++ 266AStonesOnTheTable/266AStonesOnTheTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _266AStonesOnTheTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int stones_row = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int number_stones = 0;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    number_stones++;
+                }
+            }
+
+            Console.WriteLine(number_stones);
+        }
+    }
+}
---
+++ 791ABearBigBrother/791ABearBigBrother/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _791ABearBigBrother
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] weigths = Console.ReadLine().Split(' ');
+            int limack = int.Parse(weigths[0]);
+            int bear = int.Parse(weigths[1]);
+            int years = 0;
+
+            while (limack <= bear)
+            {
+                limack *= 3;
+                bear *= 2;
+                years++;
+            }
+
+            Console.WriteLine(years);
+
+
+        }
+    }
+}
