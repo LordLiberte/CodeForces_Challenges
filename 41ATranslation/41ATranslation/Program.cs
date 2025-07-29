3fcf477ef100c14c6d02d18a307a49f36a2f549d
Lord Liberté <GhostRg110@gmail.com>
2025-07-29 14:08:16 +0200

    Migrate project to .NET 9 and simplify Program.cs
    Converted the project from .NET Framework 4.8 to .NET 9 by updating the .csproj file and removing legacy files (App.config, AssemblyInfo.cs). Refactored Program.cs to a top-level statement style, removed unnecessary namespaces, and modernized string reversal logic. Updated solution and build artifacts to match the new SDK-style project structure.

--- 41ATranslation/41ATranslation/Program.cs
+++ 41ATranslation/41ATranslation/Program.cs
@@ -1,37 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿string input1 = Console.ReadLine();
+string input2 = Console.ReadLine();
 
-namespace _41ATranslation
+int counter_coincidence = 0;
+
+if (input1.Length != input2.Length)
 {
-    internal class Program
+    Console.WriteLine("NO");
+    return;
+}
+
+for (int i = 0; i < input1.Length; i++)
+{
+    if (input1[i] == input2[^(i + 1)])
     {
-        static void Main(string[] args)
-        {
-            string input1 = Console.ReadLine().Replace(" ", "");
-            string input2 = Console.ReadLine().Replace(" ", "");
-            Console.WriteLine(input2[^1]);
-            int counter_coincidence = 0;
-
-            for (int i = 0; i < input1.Length; i++)
-            {
-                if (input1[i] == input2[-1 + (-i)])
-                {
-                    counter_coincidence++;
-                }
-            }
-
-            if (counter_coincidence == input1.Length)
-            {
-                Console.WriteLine("YES");
-            }
-            else
-            {
-                Console.WriteLine("NO");
-            }
-        }
+        counter_coincidence++;
     }
+}
+
+if (counter_coincidence == input1.Length)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
 }
