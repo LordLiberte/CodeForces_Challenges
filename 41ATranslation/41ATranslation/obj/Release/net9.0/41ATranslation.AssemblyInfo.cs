2dae655e71cc0011d6952cb631ec91fcc5733f14
Lord Liberté <GhostRg110@gmail.com>
2025-07-29 14:14:05 +0200

    Update build outputs and Visual Studio metadata
    Regenerated build artifacts for multiple projects, including new and updated .dll, .pdb, and .cache files for Debug and Release configurations. Updated Visual Studio workspace and document layout metadata, reflecting recent changes in project structure and file states.

--- 41ATranslation/41ATranslation/obj/Release/net9.0/41ATranslation.AssemblyInfo.cs
+++ 41ATranslation/41ATranslation/obj/Release/net9.0/41ATranslation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("41ATranslation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fcf477ef100c14c6d02d18a307a49f36a2f549d")]
 [assembly: System.Reflection.AssemblyProductAttribute("41ATranslation")]
