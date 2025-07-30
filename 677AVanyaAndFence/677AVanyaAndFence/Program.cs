a3ad23b97f2f60c494f4ccd7d6fd2c89f6ab9a83
Lord Liberté <GhostRg110@gmail.com>
2025-07-30 13:04:25 +0200

    Add solution for 677A Vanya and Fence in C#
    Initial commit with Visual Studio solution, project files, and implementation for the Codeforces problem 677A Vanya and Fence. The Program.cs reads input, calculates the required width, and outputs the result.

---
+++ 677AVanyaAndFence/677AVanyaAndFence/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] data = Console.ReadLine().Split(' ');
+int n = int.Parse(data[0]);
+int h = int.Parse(data[1]);
+
+int[] heights = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+int width = 0;
+
+foreach (int height in heights)
+{
+    if (height > h)
+    {
+        width += 2; // If the height is greater than h, we need 2 units of width
+    }
+    else
+    {
+        width += 1; // Otherwise, we need only 1 unit of width
+    }
+
+}
+
+Console.WriteLine(width);
