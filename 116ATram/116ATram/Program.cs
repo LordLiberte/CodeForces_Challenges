eb2867b75613c06551465c705d3bb19cfb15588e
Lord Liberté <GhostRg110@gmail.com>
2025-07-30 14:08:25 +0200

    Add solutions for 116A Tram and 271A Beautiful Year
    Added new C# projects for Codeforces problems 116A (Tram) and 271A (Beautiful Year), including solution files, project files, and initial implementations in Program.cs. Includes all necessary build and configuration files for .NET 9.0.

---
+++ 116ATram/116ATram/Program.cs
@@ -0,0 +1,25 @@
+﻿int numberStops = int.Parse(Console.ReadLine());
+int currentPassengers = 0;
+
+List<int> passengersPerStop = new List<int>();
+
+while (numberStops-- > 0)
+{
+    
+    string[] input = Console.ReadLine().Split();
+    int passengersGettingOut = int.Parse(input[0]);
+    int passengersGettingIn = int.Parse(input[1]);
+
+    // Update passengers count decreasing those getting out and increasing those getting in
+    currentPassengers += passengersGettingIn;
+    currentPassengers -= passengersGettingOut;
+
+    // Add the current number of passengers to the list
+    // The max number of passengers its the maximum number of passengers for the tram
+    passengersPerStop.Add(currentPassengers);
+
+
+}
+
+
+Console.WriteLine(passengersPerStop.Max());
---
+++ 271ABeautifulYear/271ABeautifulYear/Program.cs
@@ -0,0 +1,19 @@
+﻿string year = Console.ReadLine();
+bool isBeautiful = false;
+
+while (!isBeautiful)
+{
+    year = (int.Parse(year) + 1).ToString();
+    
+    foreach (char digit in year)
+    {
+        if (year.IndexOf(digit) != year.LastIndexOf(digit))
+        {
+            isBeautiful = false;
+            break;
+        }
+        isBeautiful = true;
+    }
+}
+
+Console.WriteLine(year);
